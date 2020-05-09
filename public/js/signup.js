e48de91bb0420ab6c3ee0ad3a06f6aa7a30ba992
Andrew Thomas <athomik79@gmail.com>
2020-05-09 13:21:33 -0400

    fixed linting issues

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,15 +1,16 @@
 $(document).ready(function() {
   // Getting references to our form and input
-  var signUpForm = $("form.signup");
-  var emailInput = $("input#email-input");
-  var passwordInput = $("input#password-input");
+  const signUpForm = $('form.signup');
+  const emailInput = $('input#email-input');
+  const passwordInput = $('input#password-input');
 
-  // When the signup button is clicked, we validate the email and password are not blank
-  signUpForm.on("submit", function(event) {
+  // When the signup button is clicked,
+  //  we validate the email and password are not blank
+  signUpForm.on('submit', function(event) {
     event.preventDefault();
-    var userData = {
+    const userData = {
       email: emailInput.val().trim(),
-      password: passwordInput.val().trim()
+      password: passwordInput.val().trim(),
     };
 
     if (!userData.email || !userData.password) {
@@ -17,26 +18,41 @@ $(document).ready(function() {
     }
     // If we have an email and password, run the signUpUser function
     signUpUser(userData.email, userData.password);
-    emailInput.val("");
-    passwordInput.val("");
+    emailInput.val('');
+    passwordInput.val('');
   });
 
-  // Does a post to the signup route. If successful, we are redirected to the members page
-  // Otherwise we log any errors
+  /**
+ * loginUser does a post to our "api/login" route and if successful,
+ * redirects us the the members page
+ *
+ * @param {string} email - The user's login email address
+ * @param {string} password - The user's login password
+ *
+ * @example
+ *
+ *     loginUser('example@gmail.com', secretpassword')
+ */
   function signUpUser(email, password) {
-    $.post("/api/signup", {
+    $.post('/api/signup', {
       email: email,
-      password: password
+      password: password,
     })
-      .then(function(data) {
-        window.location.replace("/members");
+        .then(function(data) {
+          window.location.replace('/members');
         // If there's an error, handle it by throwing up a bootstrap alert
-      })
-      .catch(handleLoginErr);
+        })
+        .catch(handleLoginErr);
   }
 
+  /**
+ * loginUser does a post to our "api/login" route and if successful,
+ * redirects us the the members page
+ *
+ * @param {string} err - The user's login email address)
+ */
   function handleLoginErr(err) {
-    $("#alert .msg").text(err.responseJSON);
-    $("#alert").fadeIn(500);
+    $('#alert .msg').text(err.responseJSON);
+    $('#alert').fadeIn(500);
   }
 });
